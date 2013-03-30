464bcc76c87d0eb6753d5456d9ed8722442f344c
tad <tad@tads-bits.(none)>
2013-03-29 22:35:02 -0500

    Added ability to filter .wav files.

--- rtproc/Python/PortAudioPipe.py
+++ rtproc/Python/PortAudioPipe.py
@@ -7,6 +7,7 @@ import select
 import sys
 import os
 import struct
+import wave
 import SoundProcessor
 
 FRAMES_PER_BUFFER = 4
@@ -29,7 +30,8 @@ effects = {
            }
 
 class PortAudioPipe:
-    def __init__(self):
+    def __init__(self, sound_file=None):
+        self._sound_file = None
         self._pa = None
         self._istream = None
         self._ostream = None
@@ -37,15 +39,79 @@ class PortAudioPipe:
         self._sound_processor = None
         self._effect = None
         self._err = 0
-            
+        self._sound_file = sound_file
+        self._frames_per_buffer = FRAMES_PER_BUFFER
+ 
     def initialize(self):
-        self._sample_block = [0.0] * FRAMES_PER_BUFFER * NUM_CHANNELS * SAMPLE_SIZE
-        
         # Initialize the PortAudio library
         self._pa = pyaudio.PyAudio()
         
+        # If we have a sound file, set up the parameters here 
+        if not self._sound_file == None:
+            self._istream = wave.open(self._sound_file, 'rb')
+            self._sample_rate = self._istream.getframerate()
+            self._sample_size = 1
+            self._frames_per_buffer = 1
+            self._num_channels = self._istream.getnchannels()
+            self._format = self._pa.get_format_from_width(self._istream.getsampwidth())
+            if self._format == pyaudio.paUInt8:
+                self._pack_format = 'B'
+            elif self._format == pyaudio.paInt8:
+                self._pack_format = 'b'                
+            elif self._format == pyaudio.paInt16:
+                self._pack_format = 'h'
+            elif self._format == pyaudio.paInt24:
+                # TODO
+                pass
+            elif self._format == pyaudio.paInt32:
+                self._pack_format = 'i'
+            elif self._format == pyaudio.paFloat32:
+                self._pack_foramt = 'f'
+        else:   
+            self._sample_rate = 44100
+            self._sample_size = 2
+            self._frames_per_buffer = 4
+            self._num_channels = 1
+            self._format = pyaudio.paInt32
+            self._pack_format = 'i'     
+            self._istream = self._pa.open(
+                                    channels=self._num_channels,
+                                    format=self._format,
+                                    rate=self._sample_rate,
+                                    frames_per_buffer=self._frames_per_buffer,
+                                    input=True)  
+                        
+        self._ostream = self._pa.open(
+                            channels=self._num_channels,
+                            format=self._format,
+                            rate=self._sample_rate,
+                            frames_per_buffer=self._frames_per_buffer,
+                            output=True) 
+        self._sample_block = [0.0] * self._frames_per_buffer * self._num_channels * self._sample_size
+ 
         # Set up the SoundProcessor
-        self._sound_processor = SoundProcessor.SoundProcessor(SR=SAMPLE_RATE)
+        self._sound_processor = SoundProcessor.SoundProcessor(SR=self._sample_rate)
+
+    def _play_sample(self):
+        try:
+            if not self._sound_file == None:
+                sample_block = self._istream.readframes(self._frames_per_buffer)
+            else:
+                sample_block = self._istream.read(self._frames_per_buffer)
+            current_block = []
+            for j in range(0,len(sample_block),self._frames_per_buffer):
+                aux = long(struct.unpack(self._pack_format, sample_block[j:j+self._frames_per_buffer])[0])
+                current_block = current_block + \
+                    [struct.pack(self._pack_format, self._sound_processor.Process(float(aux)))]
+            self._ostream.write(''.join(current_block), self._frames_per_buffer)
+        except IOError as e:
+            self._err = e.errno
+            self.xrun()
+        except struct.error:
+            # Occasionally, a sample goes out of range for the struct.pack
+            # function.  Rather than fail out, allow the processing to
+            # continue, effectively throwing this sample out
+            pass
         
     def start(self):        
         legal = False
@@ -62,45 +128,18 @@ class PortAudioPipe:
                 print "Please make a legal selection..."
                 print
                 print
-        
-        # Start the stream
-        self._istream = self._pa.open(
-                                channels=NUM_CHANNELS,
-                                format=PA_SAMPLE_TYPE,
-                                rate=SAMPLE_RATE,
-                                frames_per_buffer=FRAMES_PER_BUFFER,
-                                input=True)
-        self._ostream = self._pa.open(
-                                channels=NUM_CHANNELS,
-                                format=PA_SAMPLE_TYPE,
-                                rate=SAMPLE_RATE,
-                                frames_per_buffer=FRAMES_PER_BUFFER,
-                                output=True)
                 
-        self._istream.start_stream()
+        # Start the streams
+        if self._sound_file == None:
+            self._istream.start_stream()
         self._ostream.start_stream()
         
         # Loop until we're told to quit
         while True:
             key, _, _ = select.select([sys.stdin], [], [], 0)
             if key == []:
-                try:
-                    sample_block = self._istream.read(FRAMES_PER_BUFFER)
-                    current_block = []
-                    for j in range(0,len(sample_block),FRAMES_PER_BUFFER):
-                        aux = long(struct.unpack('i', sample_block[j:j+FRAMES_PER_BUFFER])[0])
-                        current_block = current_block + \
-                            [struct.pack('i', self._sound_processor.Process(float(aux)))]
-                    self._ostream.write(''.join(current_block), FRAMES_PER_BUFFER)
-                except IOError as e:
-                    self._err = e.errno
-                    self.xrun()
-                except struct.error:
-                    # Occasionally, a sample goes out of range for the struct.pack
-                    # function.  Rather than fail out, allow the processing to
-                    # continue, effectively throwing this sample out
-                    pass
-            else:
+                self._play_sample()
+            else:   
                 effect = sys.stdin.read(1)
                 if effect.upper() == 'Q':
                     print "Thanks for playing!"
@@ -110,10 +149,13 @@ class PortAudioPipe:
                     self._effect = effect
                     self._sound_processor.SetFunction(effect)
                     self._print_options()
+                    if not self._sound_file == None:
+                        self._istream.rewind()
         
     def stop(self):
         if not self._istream == None:
-            self._istream.stop_stream()
+            if self._sound_file == None:
+                self._istream.stop_stream()
             self._istream.close()
         if not self._ostream == None:
             self._ostream.stop_stream()
--- rtproc/Python/rtproc.py
+++ rtproc/Python/rtproc.py
@@ -3,8 +3,13 @@
 # de Lausanne.
 
 import PortAudioPipe
+import sys
 
 if __name__ == '__main__':
-    p = PortAudioPipe.PortAudioPipe()
+    sound_file = None
+    if len(sys.argv) > 1:
+        sound_file = sys.argv[1]
+
+    p = PortAudioPipe.PortAudioPipe(sound_file)
     p.initialize()
     p.start()
